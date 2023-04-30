018296086c223670db9ed2663366c02e54db7880
Haley T <hntrimpert@yahoo.com>
2023-04-30 18:30:45 -0400

    routes made

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,14 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../../controllers/userController.js");
+
+router.route("/").get(getUsers).post(createUser);
+
+router.route("/:UserId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+module.exports = router;
