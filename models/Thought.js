746adae6910e1c660da39f5dd99ed1c70b02ae32
Haley T <hntrimpert@yahoo.com>
2023-04-30 18:10:07 -0400

    models made

---
+++ models/Thought.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+  }
+);
+
+thoughtSchema.virtual("reactionsCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
