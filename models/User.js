f6f7dce44030779e1ccd1b8b61d41486a95af104
Haley T <hntrimpert@yahoo.com>
2023-05-10 15:02:55 -0400

    fixed models

--- models/User.js
+++ models/User.js
@@ -1,32 +1,40 @@
 const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: /.+\@.+\..+/,
-  },
-  thoughts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Thought",
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed: true,
     },
-  ],
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /.+\@.+\..+/,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
 
-  friends: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User",
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
     },
-  ],
-});
+    id: false,
+  }
+);
 
 userSchema.virtual("friendsCount").get(function () {
   return this.friends.length;
